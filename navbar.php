c5880fc75afec6c010b00d9499c90ecb256f8bcd
Yash <sat107kar@gmail.com>
2021-03-19 14:08:17 +0530

    Initial commit

---
+++ navbar.php
@@ -0,0 +1,63 @@
+<!-- navbar --> 
+<html>
+    <head>
+        <title>Tutorpoint</title>
+        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+        <body>
+            <header class="header">
+            <div class="bg">
+               
+                <nav class="navbar navbar-expand-sm navbar-light"> 
+                    <div class="container"> 
+                        <a class="navbar-brand text-success" href="#"> 
+                          
+                          <h1 style="color: #957bda">Sparks Bank</h1>
+                        </a> 
+                        <button class="navbar-toggler" type="button" 
+                                data-toggle="collapse"
+                                data-target="#navbarSupportedContent"
+                                aria-controls="navbarSupportedContent"
+                                aria-expanded="false" 
+                                aria-label="Toggle navigation"> 
+                            <span class="navbar-toggler-icon"></span> 
+                        </button> 
+              
+                        <div class="collapse navbar-collapse"></div> 
+              
+                        <div class="collapse navbar-collapse" 
+                             id="navbarSupportedContent"> 
+                            <ul class="navbar-nav mr-auto"> 
+                                <li class="nav-item active"> 
+                                    <a class="nav-link" 
+                                       href="index.php"> 
+                                      Home  
+                                      <span class="sr-only"> 
+                                        (current) 
+                                      </span> 
+                                  </a> 
+                                </li> 
+                                <li class="nav-item"> 
+                                    <a class="createuser.php" href="createuser.php">Create User</a> 
+                                </li> 
+                                <li class="nav-item"> 
+                                    <a class="nav-link" href="transfermoney.php">Transfer Money</a> 
+                                </li>
+                                </li> 
+                                <li class="nav-item"> 
+                                    <a class="nav-link" href="TransactionHistory.php">Transaction History</a>
+                                </li>
+                            </ul>
+                        </div> 
+                    </div> 
+                </nav>
